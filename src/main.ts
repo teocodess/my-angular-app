29e60bbb466ab24b4fcd2a8fe36eb25a1e269c5c
teocodess <teodora.v.nedyalkova@gmail.com>
2024-12-16 06:43:58 +0100

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter} from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      { path: '', loadComponent: () => import('./app/home/home.component').then(m => m.HomeComponent) }, // default loading home component, not app
+      { path: 'about', loadComponent: () => import('./app/about/about.component').then(m => m.AboutComponent) },
+      { path: 'products', loadComponent: () => import('./app/products/products.component').then(m => m.ProductsComponent) },
+      { path: 'contact', loadComponent: () => import('./app/contact/contact.component').then(m => m.ContactComponent) },
+    ]),
+  ],
+});
